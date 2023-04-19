e6c06973dbb33e10c136bdf3f9e4225d50576cd3
intrigue0707 <intrigue0707@gmail.com>
2023-04-19 16:46:45 +0300

    Update cart_with_product_opt.scala

--- src/test/scala/api/cart_with_product_opt.scala
+++ src/test/scala/api/cart_with_product_opt.scala
@@ -1,6 +1,5 @@
 package api
 
-import api.cart._
 import helpers.BaseHelpers._
 import io.gatling.core.Predef._
 import io.gatling.core.structure._
@@ -10,6 +9,6 @@ object cart_with_product_opt {
  def cartWithProductOpt(): ChainBuilder =
    exec(
      http("Cart_with_product")
-       .options(s"/api/v1/cart/${cart_id}?lang=en")
+       .options("/api/v1/cart/${cart_id}?lang=en")
    )
 }
